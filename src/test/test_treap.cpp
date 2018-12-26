72b0cbe2879759f68f52118e57ca0320751d9e30
tomtseng <thomasts@andrew.cmu.edu>
2018-12-25 18:55:56 -0800

    treap: Add small test

--- src/test/test_treap.cpp
+++ src/test/test_treap.cpp
@@ -2,15 +2,16 @@
 
 #include <gtest/gtest.h>
 
-TEST(Treap, DummyTest) {
+TEST(Treap, JoinAndSplitSingleNodes) {
   treap::Node nodes[2];
   EXPECT_NE(nodes[0].GetRoot(), nodes[1].GetRoot());
-  treap::Node::Join(&nodes[0], &nodes[1]);
-  EXPECT_EQ(nodes[0].GetRoot(), nodes[1].GetRoot());
-  nodes[0].Split();
-  EXPECT_NE(nodes[0].GetRoot(), nodes[1].GetRoot());
-}
 
-TEST(Treap, DummyFailingTest) {
-  EXPECT_EQ(1, 2);
+  treap::Node* join_result = treap::Node::Join(&nodes[0], &nodes[1]);
+  EXPECT_EQ(nodes[0].GetRoot(), nodes[1].GetRoot());
+  EXPECT_EQ(nodes[0].GetRoot(), join_result->GetRoot());
+
+  std::pair<treap::Node*, treap::Node*> split_result = nodes[0].Split();
+  EXPECT_NE(nodes[0].GetRoot(), nodes[1].GetRoot());
+  EXPECT_EQ(&nodes[0], split_result.first);
+  EXPECT_EQ(&nodes[1], split_result.second);
 }
