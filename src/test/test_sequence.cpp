fc4824bfbb1606120fc451cfd24c4e7ec4a52428
tomtseng <thomasts@andrew.cmu.edu>
2018-12-26 09:41:31 -0800

    sequence: Add test for edge cases

--- src/test/test_sequence.cpp
+++ src/test/test_sequence.cpp
@@ -16,3 +16,17 @@ TEST(Sequence, JoinAndSplitSingleElements) {
   EXPECT_EQ(&elements[0], split_result.first);
   EXPECT_EQ(&elements[1], split_result.second);
 }
+
+TEST(Sequence, JoinAndSplitEmptySequences) {
+  // Check that joining with empty sequences and splitting at the end of a
+  // sequence doesn't cause errors.
+
+  seq::Element::Join(nullptr, nullptr);
+
+  seq::Element elements[2];
+  seq::Element::Join(&elements[0], &elements[1]);
+  seq::Element::Join(&elements[1], nullptr);
+  seq::Element::Join(nullptr, &elements[1]);
+  elements[1].Split();
+  EXPECT_EQ(elements[0].GetRepresentative(), elements[1].GetRepresentative());
+}
