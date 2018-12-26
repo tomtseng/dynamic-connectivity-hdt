4c8c46af96566024d9a383b2c43b5a093ecc4cf4
tomtseng <thomasts@andrew.cmu.edu>
2018-12-26 12:10:57 -0800

    Add interface to dynamic trees

--- src/src/dynamic_trees.hpp
+++ src/src/dynamic_trees.hpp
@@ -1,4 +1,33 @@
-// TODO header comment
 #pragma once
 
-// TODO implement
+#include <cstdint>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+
+#include <sequence.hpp>
+#include <utilities.hpp>
+
+// TODO comments
+class DynamicTrees {
+ public:
+  explicit DynamicTrees(uint32_t num_vertices);
+  ~DynamicTrees();
+
+  DynamicTrees() = delete;
+  DynamicTrees(const DynamicTrees &other) = delete;
+  DynamicTrees& operator=(const DynamicTrees& other) = delete;
+  DynamicTrees(DynamicTrees&& other) noexcept = delete;
+  DynamicTrees& operator=(DynamicTrees&& other) noexcept = delete;
+
+  bool IsConnected(uint32_t u, uint32_t v);
+  void Link(uint32_t u, uint32_t v);
+  void Cut(uint32_t u, uint32_t v);
+
+ private:
+  uint32_t num_vertices_;
+  std::stack<sequence::Element*> free_elements;
+  std::vector<sequence::Element*> vertices;
+  std::unordered_map<std::pair<uint32_t, uint32_t>, sequence::Element*, IntPairHasher>
+    edges;
+};
