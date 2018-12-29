354a863580436071e4ef546795104976c3e022a5
tomtseng <thomasts@andrew.cmu.edu>
2018-12-28 20:45:45 -0800

    dynamic_forest: Add augmentations in preparation for dynamic connectivity

--- src/dynamic_graph/src/dynamic_forest.hpp
+++ src/dynamic_graph/src/dynamic_forest.hpp
@@ -11,11 +11,15 @@
 // This is a data structure for the dynamic trees problem. The dynamic trees
 // problem is to maintain a forest (undirected acyclic graph) as it undergoes
 // edge additions ("links") and edge deletions ("cuts").
+//
+// The implementation is specialized for use in Holm et al.'s dynamic
+// connectivity algorithm, which is why we have the functions `MarkEdge()` and
+// `MarkVertex()`.
 class DynamicForest {
  public:
   // Initializes forest with `num_vertices` vertices and no edges.
   //
-  // Efficiency: Linear in the size of the forest.
+  // Efficiency: linear in the size of the forest.
   explicit DynamicForest(int64_t num_vertices);
   ~DynamicForest();
 
@@ -28,22 +32,43 @@ class DynamicForest {
   // Returns true if vertices `u` and `v` are connected, i.e. are in the same
   // tree.
   //
-  // Efficiency: Logarithmic in the size of the forest.
-  bool IsConnected(int64_t u, int64_t v);
+  // Efficiency: logarithmic in the size of the forest.
+  bool IsConnected(int64_t u, int64_t v) const;
 
   // Adds edge between vertices `u` and `v`.
   //
   // Adding this edge must not create a cycle in the forest.
   //
-  // Efficiency: Logarithmic in the size of the forest.
+  // Efficiency: logarithmic in the size of the forest.
   void AddEdge(int64_t u, int64_t v);
 
   // Removes edge between vertices `u` and `v`.
   //
   // The edge (`u`, `v`) must be in the forest.
   //
-  // Efficiency: Logarithmic in the size of the forest.
+  // Efficiency: logarithmic in the size of the forest.
   void DeleteEdge(int64_t u, int64_t v);
+
+  // Returns size of the tree that vertex `v` resides in.
+  //
+  // Efficiency: logarithmic in the size of the forest.
+  int64_t GetSizeOfTree(int64_t v) const;
+
+  // Mark (if `mark` is true) or unmark (if `mark` is false) an edge. See
+  // `GetMarkedEdgeInTree`.
+  //
+  // Efficiency: logarithmic in the size of the forest.
+  void MarkEdge(int64_t u, int64_t v, bool mark);
+  // Analagous to `GetEdge`.
+  void MarkVertex(int64_t v, bool mark);
+  // Finds an edge in the tree that vertex `v` resides in that was marked by
+  // `MarkEdge(., ., true)`
+  //
+  // Efficiency: logarithmic in the size of the forest.
+  std::optional<std::pair<int64_t, int64_t>>
+  GetMarkedEdgeInTree(int64_t v) const;
+  // Analagous to `GetMarkedVertexInTree`.
+  std::optional<int64_t> GetMarkedVertexInTree(int64_t v) const;
 
  private:
   sequence::Element* AllocateEdgeElement(int64_t u, int64_t v);
--- src/dynamic_graph/test/test_dynamic_forest.cpp
+++ src/dynamic_graph/test/test_dynamic_forest.cpp
@@ -1,61 +1,102 @@
 #include <dynamic_forest.hpp>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+using ::testing::Optional;
+using ::testing::Pair;
+
 TEST(DynamicForest, AddEdgeAndDeleteEdgePathGraph) {
-  constexpr uint64_t kNumVertices = 10;
+  constexpr int64_t kNumVertices = 10;
   DynamicForest dynamic_forest(kNumVertices);
-  for (uint64_t i = 1; i < kNumVertices; i++) {
+  for (int64_t i = 1; i < kNumVertices; i++) {
     dynamic_forest.AddEdge(i - 1, i);
   }
-  for (uint64_t i = 1; i < kNumVertices; i++) {
+  for (int64_t i = 1; i < kNumVertices; i++) {
     EXPECT_TRUE(dynamic_forest.IsConnected(0, i));
   }
 
   dynamic_forest.DeleteEdge(4, 5);
   EXPECT_FALSE(dynamic_forest.IsConnected(4, 5));
-  for (uint64_t i = 0; i < 4; i++) {
+  for (int64_t i = 0; i < 4; i++) {
     EXPECT_TRUE(dynamic_forest.IsConnected(4, i));
   }
-  for (uint64_t i = 5; i < kNumVertices; i++) {
+  for (int64_t i = 5; i < kNumVertices; i++) {
     EXPECT_TRUE(dynamic_forest.IsConnected(5, i));
   }
 
-  for (uint64_t i = 1; i < kNumVertices; i++) {
+  for (int64_t i = 1; i < kNumVertices; i++) {
     if (i != 5) {
       dynamic_forest.DeleteEdge(i - 1, i);
     }
   }
-  for (uint64_t i = 0; i < kNumVertices; i++) {
-    for (uint64_t j = i + 1; j < kNumVertices; j++) {
+  for (int64_t i = 0; i < kNumVertices; i++) {
+    for (int64_t j = i + 1; j < kNumVertices; j++) {
       EXPECT_FALSE(dynamic_forest.IsConnected(i, j));
     }
   }
 }
 
 TEST(DynamicForest, AddEdgeAndDeleteEdgeStarGraph) {
-  constexpr uint64_t kNumVertices = 10;
+  constexpr int64_t kNumVertices = 10;
   DynamicForest dynamic_forest(kNumVertices);
-  for (uint64_t i = 1; i < kNumVertices; i++) {
+  for (int64_t i = 1; i < kNumVertices; i++) {
     dynamic_forest.AddEdge(0, i);
   }
-  for (uint64_t i = 1; i < kNumVertices; i++) {
+  for (int64_t i = 1; i < kNumVertices; i++) {
     EXPECT_TRUE(dynamic_forest.IsConnected(0, i));
   }
 
   dynamic_forest.DeleteEdge(0, 5);
-  for (uint64_t i = 0; i < kNumVertices; i++) {
+  for (int64_t i = 0; i < kNumVertices; i++) {
     EXPECT_EQ(dynamic_forest.IsConnected(0, i), i != 5);
   }
 
-  for (uint64_t i = 1; i < kNumVertices; i++) {
+  for (int64_t i = 1; i < kNumVertices; i++) {
     if (i != 5) {
       dynamic_forest.DeleteEdge(0, i);
     }
   }
-  for (uint64_t i = 0; i < kNumVertices; i++) {
-    for (uint64_t j = i + 1; j < kNumVertices; j++) {
+  for (int64_t i = 0; i < kNumVertices; i++) {
+    for (int64_t j = i + 1; j < kNumVertices; j++) {
       EXPECT_FALSE(dynamic_forest.IsConnected(i, j));
     }
   }
+}
+
+TEST(DynamicForest, Mark) {
+  DynamicForest dynamic_forest(10);
+
+  dynamic_forest.MarkVertex(8, true);
+  for (int32_t i = 1; i < 10; i++) {
+    dynamic_forest.AddEdge(i - 1, i);
+  }
+  EXPECT_FALSE(dynamic_forest.GetMarkedEdgeInTree(0).has_value());
+  EXPECT_THAT(dynamic_forest.GetMarkedVertexInTree(0), Optional(8));
+
+  dynamic_forest.MarkEdge(3, 2, true);
+  EXPECT_THAT(dynamic_forest.GetMarkedEdgeInTree(0), Optional(Pair(3, 2)));
+
+  dynamic_forest.MarkEdge(6, 7, true);
+  dynamic_forest.DeleteEdge(3, 2);
+  EXPECT_FALSE(dynamic_forest.GetMarkedEdgeInTree(0).has_value());
+  EXPECT_FALSE(dynamic_forest.GetMarkedVertexInTree(0).has_value());
+  EXPECT_THAT(dynamic_forest.GetMarkedEdgeInTree(9), Optional(Pair(6, 7)));
+  EXPECT_THAT(dynamic_forest.GetMarkedVertexInTree(9), Optional(8));
+
+  dynamic_forest.MarkEdge(6, 7, false);
+  EXPECT_FALSE(dynamic_forest.GetMarkedEdgeInTree(9).has_value());
+
+  dynamic_forest.MarkVertex(8, false);
+  dynamic_forest.MarkVertex(1, true);
+  EXPECT_FALSE(dynamic_forest.GetMarkedVertexInTree(9).has_value());
+  EXPECT_THAT(dynamic_forest.GetMarkedVertexInTree(0), Optional(1));
+
+  dynamic_forest.MarkEdge(6, 7, true);
+  dynamic_forest.AddEdge(3, 2);
+  EXPECT_THAT(dynamic_forest.GetMarkedEdgeInTree(0), Optional(Pair(6, 7)));
+  EXPECT_THAT(dynamic_forest.GetMarkedVertexInTree(9), Optional(1));
+
+  dynamic_forest.MarkEdge(6, 7, false);
+  EXPECT_FALSE(dynamic_forest.GetMarkedEdgeInTree(0).has_value());
 }
